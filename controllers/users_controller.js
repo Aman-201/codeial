82d1d0442b2d21de7005511da8d5e1f60f547233
Aman-201 <81082654+Aman-201@users.noreply.github.com>
2022-07-19 21:11:59 +0530

    sign up and saving in db done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,5 @@
 const User=require('../models/user');
-
+const bodyParser=require('body-parser')
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -24,8 +24,10 @@ module.exports.signIn = function(req, res){
 
 // get the sign up data
 module.exports.create = function(req, res){
-if(req.body.password!==req.body.confirmpassword)
+    console.log(req.body);
+if(req.body.password!=req.body.confirm_password)
 {
+    console.log("password not equal");
    return res.redirect('back');
 }
 
@@ -34,15 +36,18 @@ User.findOne({'email':req.body.email},function(err,result)
     if(err){ console.log("error in finding the user");}
     if(!result)
     {
+        console.log("added to databasee");
         User.create(req.body,function(req,res)
         {
             if(err){
                 console.log("error in signing up");
             }
-            return res.redirect('/users/create');
+           
         })
+        return res.redirect('/users/create');
     }
     else{
+        // console.log(result);
         return res.redirect('back');
     }
 })
--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const cookieParser=require('cookie-parser');
+const bodyParser=require('body-parser');
 
 app.use(express.static('./assets'));
-
+app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressLayouts);
 app.use(cookieParser());
 // extract style and scripts from sub pages into the layout
